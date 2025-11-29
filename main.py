d64592a427c44d92bd8c75dd1286c0275be84034
kate <fkaty020311@gmail.com>
2025-11-29 13:32:57 +0300

    круги случайного цвета

--- main.py
+++ main.py
@@ -1,23 +1,57 @@
 import sys
 import random
 
-from PyQt6 import uic
+from PyQt6 import QtCore, QtGui, QtWidgets
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtWidgets import QApplication, QMainWindow
 
 
-class Circles(QMainWindow):
+# pyuic6 UI.ui -o UI.py (в терминал)
+# из учебника QT 2. QtDesigner, pyuic, два способа подключения uic-файла
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(600, 600)
+        self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Policy.Minimum,
+                                           QtWidgets.QSizePolicy.Policy.Expanding)
+        self.verticalLayout.addItem(spacerItem)
+        self.pushButton = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(parent=MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 600, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(parent=MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Нарисовать круг"))
+
+
+class Circles(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.circle)
         self.data = []
 
     def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
-        qp.setBrush(QColor(255, 255, 0))
-        for x, y, w in self.data:
+        for x, y, w, col in self.data:
+            qp.setBrush(col)
             qp.drawEllipse(x, y, w, w)
         qp.end()
 
@@ -25,7 +59,8 @@ class Circles(QMainWindow):
         w = random.randint(3, 350)
         x = random.randint(1, 600 - w)
         y = random.randint(1, 600 - w)
-        self.data.append((x, y, w))
+        col = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.data.append((x, y, w, col))
         self.update()
 
 
